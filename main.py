54d0de7f0cb704fb7b0600e27c743b2bed729044
Amit <87896992+enigmatic-cipher@users.noreply.github.com>
2022-01-16 09:45:08 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+"""
+Given n as input. Print 1 to n (in following format) on screen using a For loop.
+Output Format:
+Start
+1
+2
+...
+n
+End
+"""
+
+n = 3
+print("Start")
+for i in range(1,n+1):
+  print(i)
+print("End")
